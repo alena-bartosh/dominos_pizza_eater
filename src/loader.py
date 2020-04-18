f9c8baf8a21480619be786811adbb81aa25b1d43
Alena Bartosh <olena.bartosh@lotusflare.com>
2020-04-18 19:01:12 +0300

    Add --no-close arg

--- src/loader.py
+++ src/loader.py
@@ -7,9 +7,12 @@ parser = argparse.ArgumentParser(description='Script that loads data from domino
 parser.add_argument('--lang', type=str, default='ru', help='Site and data language')
 parser.add_argument('-l', '--login', type=str, help='User\'s login or email')
 parser.add_argument('-p', '--password', type=str, help='User\'s password')
+parser.add_argument('-n', '--no-close', action='store_true')
 
 args = parser.parse_args()
 
 driver = webdriver.Chrome()
 driver.get(f'https://dominos.ua/{args.lang}/kyiv/')
-driver.close()
+
+if not args.no_close:
+    driver.close()
